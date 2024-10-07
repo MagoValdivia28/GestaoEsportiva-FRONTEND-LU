cf238b322c9c96af9f3d74d14a9fb8fc41b9d979
Matheus Zambon da Costa <matheus-costa@aluno.senai.br>
2024-10-07 11:48:58 -0300

    Refactor page component styles and remove unused imports

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,18 +4,21 @@ import styles from './page.module.css';
 const Login = () => {
   return (
     <div className={styles.divpai}>
-      <h1>Log In</h1>
+
       <form className={styles.form}>
-        <div className={styles.formGroup}>
-          <label htmlFor="email">Email:</label>
+        <h1 id={styles.h1}>Log In</h1>
+        <div className={styles.formdiv1}>
+          <label htmlFor="email">Email</label>
           <input type="email" id="email" name="email" required />
         </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="password">Senha:</label>
+        <div className={styles.formdiv2}>
+          <label htmlFor="password">Senha</label>
           <input type="password" id="password" name="password" required />
         </div>
         <button type="submit" className={styles.submitButton}>Entrar</button>
       </form>
+
+
     </div>
   );
 };
