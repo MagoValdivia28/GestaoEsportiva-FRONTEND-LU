404b73fade15b6575aec01e7ad9f2ab567a395ce
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-10-07 11:54:48 -0300

    Refactor page component and update CSS styles

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,16 @@
 
+import Image from 'next/image'
+import styles from './page.module.css'
 export default function Home() {
   return (
-    <div>
+    <main className={styles.container}>
+        <div className={styles.opacity}>
+            <div>
+                <Image src="/logo.png" alt="logo" width={100} height={100} />
+            </div>
         <p>ahdsgagsdahsk</p>
-    </div>
+        </div>
+    </main>
   )
 }
 
