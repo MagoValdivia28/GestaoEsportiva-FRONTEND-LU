0ab5d2a3601dad331531027ae6f7234503de60a0
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-10-09 08:40:56 -0300

    Refactorar estilos da página de login e ajustar tamanho do logo

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,21 @@
-
 import Image from 'next/image'
+import logo from './assets/imagens/logo.jpg'
 import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.container}>
         <div className={styles.opacity}>
-            <div>
-                <Image src="/aapm.png" alt="logo" width={100} height={100} />
+            <div className={styles.top}>
+                <Image src={logo} alt="Logo" width={100} height={100} />
+                <h1>Logo</h1>
             </div>
-        <p>ahdsgagsdahsk</p>
+            <div>
+                <h1>Seja bem-vindo!</h1>
+                <h2>Entre com sua conta</h2>
+            </div>
+            <div>
+              <button className={styles.button}>Conheça-nos</button>
+            </div>
         </div>
     </main>
   )
