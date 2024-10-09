501d710d30c53144e9d0c7c255fc384e46c1c3cb
Matheus Gomes <matheus.goncalves5@aluno.senai.br>
2024-10-09 08:41:04 -0300

    Refactor file paths for login page and background image

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,5 @@
 import styles from './page.module.css';
-import logo from "../assets/imagens/logo.jpg";
+import logo from "../../../assets/images/logo.jpg";
 import Image from 'next/image';
 
 const Login = () => {
