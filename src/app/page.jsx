d504c2cb49084dfb4b8ea69dc7d20e4baeac0208
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-10-14 17:05:39 -0300

    Refactoriza componentes para gerenciamento de esportes: atualiza logo e melhora tratamento de erros

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,16 +8,29 @@ export default function Home() {
   const handleProfileClick = () => {
     alert('Perfil de safado clicked!');
   };
-  const handleconhecanosClick = () => {
-    alert('conheca-nos filho da puta clicked!');
+  
+  const handleConhecaNosClick = () => {
+    alert('Conheça-nos filho da puta clicked!');
   };
-
+  
+  // Funções para os cards
+  const handleGerenciamentoEquipeClick = () => {
+    alert('Gerenciamento de Equipe clicked!');
+  };
+  
+  const handleGerenciamentoAtividadesClick = () => {
+    alert('Gerenciamento de Atividades Esportivas clicked!');
+  };
+  
+  const handleGerenciamentoComentariosClick = () => {
+    alert('Gerenciamento de Comentários clicked!');
+  };
+  
   return (
     <main className={styles.container}>
       <div className={styles.opacity}>
         <div className={styles.top}>
-          <Image src={logo} alt="Logo" width={100} height={100} className={styles.logo}
-          />
+          <Image src={logo} alt="Logo" width={100} height={100} className={styles.logo} />
           <button className={styles.profileButton} onClick={handleProfileClick}>
             <FaUser size={20} className={styles.pb2} />
           </button>
@@ -28,28 +41,29 @@ export default function Home() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id finibus ipsum.
             Phasellus dapibus sed massa et finibus. Lorem ipsum dolor sit amet, consectetur
             adipiscing elit. Duis id finibus ipsum. Phasellus dapibus sed massa et finibus.
-            Sed bibendum turpis et lectus tempor pulvinar. Sed bibendum turpis et lectus tempor pulvinar.
+            Sed bibendum turpis et lectus tempor pulvinar.
           </p>
         </div>
         <div className={styles.divbutton}>
-          <button className={styles.button} onClick={handleconhecanosClick} >Conheça-nos</button>
+          <button className={styles.button} onClick={handleConhecaNosClick}>Conheça-nos</button>
         </div>
       </div>
-
+  
       <div className={styles.container2}>
-        <div className={styles.cards}>
-          <FaUsers size={30} />
-          <p>Gerenciamento de Equipe</p>
+        <div className={styles.cards} onClick={handleGerenciamentoEquipeClick}>
+          <FaUsers size={30} className={styles.iconC2} />
+          <p className={styles.p2}>Gerenciamento de Equipe</p>
         </div>
-        <div className={styles.cards}>
-          <FaTrophy size={30} />
-          <p>Gerenciamento de Atividades Esportivas</p>
+        <div className={styles.cards} onClick={handleGerenciamentoAtividadesClick}>
+          <FaTrophy size={30} className={styles.iconC2} />
+          <p className={styles.p2}>Gerenciamento de Atividades Esportivas</p>
         </div>
-        <div className={styles.cards}>
-          <FaComments size={30} /> 
-          <p>Gerenciamento de Comentários</p>
+        <div className={styles.cards} onClick={handleGerenciamentoComentariosClick}>
+          <FaComments size={30} className={styles.iconC2} />
+          <p className={styles.p2}>Gerenciamento de Comentários</p>
         </div>
       </div>
     </main>
+  
   );
 }
