bb6a3e31aec18bcb77b38a15c5a4816a929a630c
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-10-16 09:46:48 -0300

    feat: animation on error

--- src/app/components/PopUpError/index.jsx
+++ src/app/components/PopUpError/index.jsx
@@ -1,8 +1,11 @@
 import styles from './page.module.css';
+import { BiErrorAlt } from "react-icons/bi";
+
 const PopUpError = ({ error }) => {
   return (
     <div className={styles.popup}>
       <div className={styles.popup_inner}>
+        <BiErrorAlt className={styles.icon} color='#fff' size={30} />
         <h2 className={styles.h2}>{error}</h2>
       </div>
     </div>
