6f1e58ceaba1faf5f2af64a576dbfcd569cee1c7
Matheuscosta06 <matheus-costa@aluno.senai.br>
2024-10-21 10:41:47 -0300

    feat: add CadastroCampeonato page component and styles

--- src/app/cadastrocampeonato/page.jsx
+++ src/app/cadastrocampeonato/page.jsx
@@ -2,19 +2,19 @@
 import styles from './page.module.css';
 import Image from 'next/image';
 import logo from "../../../assets/imagens/logo.png";
+import aapm from "../../../assets/imagens/aapm.png";
 import { FaUser } from 'react-icons/fa';
-import { MdOutlineUploadFile } from "react-icons/md";
 import { LiaArrowCircleLeftSolid } from "react-icons/lia";
 import Link from 'next/link';
 
-const CadastroEsportes = () => {
+const CadastroCampeonato = () => {
 
   const handleProfileClick = () => {
     alert('Perfil clicado!');
   };
 
   return (
-    <div className={styles.divpai}>
+    <div className={styles.container}>
       <Image src={logo} id={styles.logo} width={100} height={100} />
       <button className={styles.profileButton} onClick={handleProfileClick}>
         <FaUser size={20} />
@@ -24,23 +24,27 @@ const CadastroEsportes = () => {
           <Link className={styles.backButton} href={"./"}>
             <LiaArrowCircleLeftSolid size={30} />
           </Link>
-          <p className={styles.txt1}>Gerenciamento de atividades desportivas</p>
+          <p className={styles.txt1}>Gerenciamento de Campeonatos</p>
         </div>
 
         <div className={styles.formdiv1}>
           <label htmlFor="nome">Nome</label>
           <input type="text" id="nome" name="nome" required />
         </div>
-        <div className={styles.formdiv2}>
-          <label htmlFor="descricao">Tipo</label>
-          <input type="text" id="descricao" name="descricao" required />
+
+        <div className={styles.divdatas}>
+          <div className={styles.formdiv2}>
+            <label htmlFor="descricao">Data início</label>
+            <input type="text" id="descricao" name="descricao" required />
+          </div>
+          <div className={styles.formdiv3}>
+            <label htmlFor="descricao">Data Término</label>
+            <input type="text" id="descricao" name="descricao" required />
+          </div>
         </div>
 
-        <p className={styles.txt}>Upload de Imagem</p>
+        <Image className={styles.img} src={aapm} id={styles.logo} width={650} height={480} />
 
-        <button className={styles.putpicture}>
-          <MdOutlineUploadFile size={90} />
-        </button>
 
         <button type="submit" className={styles.submitButton}>Cadastrar</button>
       </form>
@@ -48,4 +52,4 @@ const CadastroEsportes = () => {
   );
 };
 
-export default CadastroEsportes;
+export default CadastroCampeonato;
