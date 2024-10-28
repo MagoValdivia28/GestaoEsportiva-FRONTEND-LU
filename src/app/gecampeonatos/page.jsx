7589ea1f3c07676f60157f286bd46126dbef7f1d
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-10-28 16:24:53 -0300

    Refactor CSS in historico page module

--- src/app/gecampeonatos/page.jsx
+++ src/app/gecampeonatos/page.jsx
@@ -1,77 +1,69 @@
 'use client';
+import React, { useEffect, useState } from 'react';
 import styles from './page.module.css';
 import hometopo from '../../../assets/imagens/hometopo.png';
+import historico from '../../../assets/imagens/historico.jpg';
+import adicionar from '../../../assets/imagens/adicionar.jpg';
 import logo from '../../../assets/imagens/logo.png';
 import Image from 'next/image';
 import { FaUser } from 'react-icons/fa';
 import Card from '../components/Card/page';
-import { useRouter } from 'next/navigation'; // Importando useRouter
-import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { getCampeonatoByDate } from '@/src/actions/campeonato';
 import PopUpError from '@/src/app/components/PopUpError';
 
-export default function Home() {
-  const router = useRouter(); // Inicializando o roteador
+const Home = () => {
+  const router = useRouter();
   const [error, setError] = useState(null);
-  const [campeonato, setCampeonato] = useState(null);
+  const [campeonato, setCampeonato] = useState([]);
 
   useEffect(() => {
-    const getCampeonato = async () => {
+    const fetchCampeonato = async () => {
       const date = new Date();
-      const year = date.getFullYear();
-      const month = date.getMonth() + 1; // Adiciona 1 porque os meses são zero-indexados
-      const day = date.getDate();
-      const fullDate = `${year}-${month}-${day}`;
-      const response = await getCampeonatoByDate(fullDate);
+      const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+      const response = await getCampeonatoByDate(formattedDate);
       if (response.message) {
         setError(response);
-        setTimeout(() => {
-          setError(null);
-        }, 1500);
+        setTimeout(() => setError(null), 1500);
       } else {
         setCampeonato(response);
       }
     };
-    getCampeonato();
+    fetchCampeonato();
   }, []);
 
-  // Função para lidar com o clique no card "Adicionar"
-  const handleAddClick = () => {
-    router.push('/cadastrocampeonato'); // Redirecionando para a página de cadastro
-  };
-  const handlehistorico = () => {
-    router.push('/historico');
-  }
+  const handleAddClick = () => router.push('/cadastrocampeonato');
+  const handleHistoricoClick = () => router.push('/historico');
 
   return (
-    <>
-      <div className={styles.container}>
-        <header className={styles.header}>
-          <Image src={logo} className={styles.logo} width={130} height={130} />
-          <div className={styles.profileIcon}>
-            <button className={styles.profileButton}>
-              <FaUser size={20} />
-            </button>
-          </div>
-        </header>
-        <main className={styles.main}>
-          <h2>Gerenciamento de atividades desportivas</h2>
-          <hr className={styles.separator} />
-          <div className={styles.cardContainer}>
-            <Card title="Adicionar" imageUrl={hometopo} onClick={handleAddClick} /> {/* Passando o manipulador de clique */}
-            <Card title="Historico" imageUrl={hometopo} />
-            <Card title="Interclasse" imageUrl={hometopo} />
-            {
-              campeonato && (
-                campeonato.map((item) => (
-                  <Card key={item.id} title={item.titulo} imageUrl={hometopo} />
-                ))
-              )
-            }
-          </div>
-        </main>
-      </div>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <Image src={logo} alt="Logo" className={styles.logo} width={130} height={130} />
+        <button className={styles.profileButton}>
+          <FaUser size={20} />
+        </button>
+      </header>
+      
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <span className={styles.titleRed}>Gerenciamento</span>
+          <span className={styles.titleBlack}> de Atividades</span>
+        </h1>
+        <div className={styles.line}></div>
+        
+        <div className={styles.cardsContainer}>
+          <Card title="Adicionar" imageUrl={adicionar} onClick={handleAddClick} />
+          <Card title="Histórico" imageUrl={historico} onClick={handleHistoricoClick} />
+          <Card title="Interclasse" imageUrl={hometopo} />
+          {campeonato.map(item => (
+            <Card key={item.id} title={item.titulo} imageUrl={hometopo} />
+          ))}
+        </div>
+      </main>
+
       {error && <PopUpError error={error} />}
-    </>
+    </div>
   );
-}
+};
+
+export default Home;
