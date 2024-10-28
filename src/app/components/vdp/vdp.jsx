13d81b2e48c91dabed94a0d0bc95eee9c89302aa
Fervalinhos <matths.henrique@yahoo.com>
2024-10-28 15:26:48 -0300

    feat: refactor VDP component structure and improve layout

--- src/app/components/vdp/vdp.jsx
+++ src/app/components/vdp/vdp.jsx
@@ -8,40 +8,40 @@ import { useEffect, useState } from 'react';
 const VDP = () => {
     return (
         <ul className={styles.placar}>
-        <div className={styles.redline}></div>
-        <div className={styles.equipesXPontos}>
-            <h3 className={styles.infoPlacar}>Equipes</h3>
-            <div className={styles.VDP_container}>
-                <h3 className={styles.VDP}>Vitorias</h3>
-                <h3 className={styles.VDP}>Derrotas</h3>
-                <h3 className={styles.VDP}>Pontos</h3>
-            </div>
-        </div>
-        <li className={styles.equipes}>
-            <div className={styles.info_container}>
-                <h3 className={styles.info}>Equipe 01</h3>
-                <div className={styles.VDPinfo_container}>
-                    <h3 className={styles.VDPinfo}>00</h3>
-                    <h3 className={styles.VDPinfo}>00</h3>
-                    <h3 className={styles.VDPinfo}>000</h3>
+            <div className={styles.redline}></div>
+            <div className={styles.equipesXPontos}>
+                <h3 className={styles.infoPlacar}>Equipes</h3>
+                <div className={styles.VDP_container}>
+                    <h3 className={styles.VDP}>Vitorias</h3>
+                    <h3 className={styles.VDP}>Derrotas</h3>
+                    <h3 className={styles.VDP}>Pontos</h3>
                 </div>
             </div>
-            <div className={styles.grayline}></div>
-        </li>
-
-
-        <li className={styles.equipes}>
-            <div className={styles.info_container}>
-                <h3 className={styles.info}>Equipe 01</h3>
-                <div className={styles.VDPinfo_container}>
-                    <h3 className={styles.VDPinfo}>00</h3>
-                    <h3 className={styles.VDPinfo}>00</h3>
-                    <h3 className={styles.VDPinfo}>000</h3>
+            <li className={styles.equipes}>
+                <div className={styles.info_container}>
+                    <h3 className={styles.info}>Equipe 01</h3>
+                    <div className={styles.VDPinfo_container}>
+                        <h3 className={styles.VDPinfo}>00</h3>
+                        <h3 className={styles.VDPinfo}>00</h3>
+                        <h3 className={styles.VDPinfo}>000</h3>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.grayline}></div>
-        </li>
-    </ul>
+                <div className={styles.grayline}></div>
+            </li>
+
+
+            <li className={styles.equipes}>
+                <div className={styles.info_container}>
+                    <h3 className={styles.info}>Equipe 01</h3>
+                    <div className={styles.VDPinfo_container}>
+                        <h3 className={styles.VDPinfo}>00</h3>
+                        <h3 className={styles.VDPinfo}>00</h3>
+                        <h3 className={styles.VDPinfo}>000</h3>
+                    </div>
+                </div>
+                <div className={styles.grayline}></div>
+            </li>
+        </ul>
     )
 }
 
