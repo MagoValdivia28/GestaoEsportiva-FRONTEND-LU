1c1ba745cdab478d12be0c891778b0f9f92643ce
Matheuscosta06 <matheus-costa@aluno.senai.br>
2024-10-30 10:54:27 -0300

    Refactorizar página Sobre para adicionar logo e ícone de perfil

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -8,11 +8,19 @@ import fervas from '../../../assets/imagens/fervas.jpeg';
 import night from '../../../assets/imagens/Night.jpeg';
 import fp from '../../../assets/imagens/fp.jpeg';
 import pravel from '../../../assets/imagens/pravel.jpeg';
+import logo from "../../../assets/imagens/logo.png";
+import { FaUser } from 'react-icons/fa';
 
 export default function Sobre() {
   return (
     <div className={styles.container}>
-      <div className={styles.cardPrincipal}>
+      <Image src={logo} id={styles.logo} width={130} height={130} />
+      <div className={styles.profileIcon}>
+        <button className={styles.profileButton}>
+          <FaUser size={20} />
+        </button>
+      </div>
+      <div className={styles.textsobre}>
         <img className={styles.fotoRS} />
         <h1 className={styles.sobreA}>Sobre a</h1>
         <h2 className={styles.novaG}>NovaGenix</h2>
@@ -25,92 +33,117 @@ export default function Sobre() {
 
 
         <div className={styles.cardImg}>
-          <Image src={equipe} className={styles.fotoEquipe} width={100} height={190} />
+          <Image src={equipe} className={styles.fotoEquipe} />
 
         </div>
         <hr className={styles.linha} />
 
+        <div className={styles.div_cards}>
 
 
-        {/* <Image className={styles.fotoMembro} src={fp} width={80} height={190} /> */}
-
-        <div className={styles.card}>
-          <div className={styles.img_container}>
-            <Image className={styles.fotoMembro} src={fp} width={80} height={190} />
-          </div>
-
-          <div className={styles.info_card}>
-            <div className={styles.texts_container}>
-              <h4 className={styles.nomeMembro}>Felipe Pedro</h4>
-              <p className={styles.membro}>membro</p>
-              <p className={styles.textMembro}>
-                Felipe é um membro com uma personalidade forte e um espírito de
-                liderança marcante, ele se destaca em qualquer desafio que enfrenta.
-                Fazendo dele uma peça chave para o nosso sucesso coletivo.
-              </p>
+          <div className={styles.card}>
+            <div className={styles.img_container}>
+              <Image className={styles.fotoMembro} src={night} />
             </div>
+
+            <div className={styles.info_card}>
+              <div className={styles.texts_container}>
+                <h4 className={styles.nomeMembro}>João Victor</h4>
+                <p className={styles.membro}>membro</p>
+                <p className={styles.textMembro}>
+                  João é um dos membros mais criativos e dedicados da nossa equipe.
+                  Com uma mente inovadora e um comprometimento exemplar, João traz
+                  ideias frescas e soluções eficazes. Sua dedicação incansável é uma
+                  inspiração para todos nós.
+                </p>
+              </div>
+            </div>
+
           </div>
 
+          <div className={styles.card}>
+            <div className={styles.img_container}>
+              <Image className={styles.fotoMembro} src={fp} />
+            </div>
+
+            <div className={styles.info_card}>
+              <div className={styles.texts_container}>
+                <h4 className={styles.nomeMembro}>Felipe Pedro</h4>
+                <p className={styles.membro}>membro</p>
+                <p className={styles.textMembro}>
+                  Felipe é um membro com uma personalidade forte e um espírito de
+                  liderança marcante, ele se destaca em qualquer desafio que enfrenta.
+                  Fazendo dele uma peça chave para o nosso sucesso coletivo.
+                </p>
+              </div>
+            </div>
+
+          </div>
+
+          <div className={styles.card}>
+            <div className={styles.img_container}>
+              <Image className={styles.fotoMembro} src={zambon} />
+            </div>
+
+            <div className={styles.info_card}>
+              <div className={styles.texts_container}>
+                <h4 className={styles.nomeMembro}>Matheus Zambon</h4>
+                <p className={styles.membro}>membro</p>
+                <p className={styles.textMembro}>
+                  Matheus Zambon é o membro mais animado e o melhor em trabalho em
+                  equipe. Sua energia contagiante e habilidade para colaborar
+                  eficazmente com todos tornam ele um elemento essencial para o nosso
+                  sucesso coletivo.
+                </p>
+              </div>
+            </div>
+
+          </div>
+
+          <div className={styles.card}>
+            <div className={styles.img_container}>
+              <Image className={styles.fotoMembro} src={pravel} />
+            </div>
+
+            <div className={styles.info_card}>
+              <div className={styles.texts_container}>
+                <h4 className={styles.nomeMembro}>Matheus Gomes</h4>
+                <p className={styles.membro}>membro</p>
+                <p className={styles.textMembro}>
+                  Matheus Gomes é um verdadeiro gênio em programação. Com um talento
+                  excepcional para desenvolver soluções, ele combina brilhantismo
+                  técnico com uma abordagem colaborativa que enriquece todo o time.
+                </p>
+              </div>
+            </div>
+
+          </div>
+
+          <div className={styles.card}>
+            <div className={styles.img_container}>
+              <Image className={styles.fotoMembro} src={fervas} />
+            </div>
+
+            <div className={styles.info_card}>
+              <div className={styles.texts_container}>
+                <h4 className={styles.nomeMembro}>Matheus Carvalho</h4>
+                <p className={styles.membro}>membro</p>
+                <p className={styles.textMembro}>
+                  Matheus Carvalho é o mestre da lógica na nossa equipe. Com um
+                  conhecimento profundo em diversas tecnologias, ele é a referência na
+                  resolução de problemas complexos. Sua expertise técnica é
+                  indispensável para o nosso sucesso.
+                </p>
+              </div>
+            </div>
+
+          </div>
         </div>
 
-        <div className={styles.backImg}>
-          <Image className={styles.fotoMembro} src={night} width={80} height={190} />
-        </div>
-        <div className={styles.cardDetalhe}>
-          <h4 className={styles.nomeMembro}>João Victor</h4>
-          <p className={styles.membro}>membro</p>
-          <p className={styles.textMembro}>
-            João é um dos membros mais criativos e dedicados da nossa equipe.
-            Com uma mente inovadora e um comprometimento exemplar, João traz
-            ideias frescas e soluções eficazes. Sua dedicação incansável é uma
-            inspiração para todos nós.
-          </p>
-        </div>
 
-
-        <div className={styles.backImg}>
-          <Image className={styles.fotoMembro} src={fervas} width={80} height={190} />
-        </div>
-        <div className={styles.cardDetalhe}>
-          <h4 className={styles.nomeMembro}>Matheus Carvalho</h4>
-          <p className={styles.membro}>membro</p>
-          <p className={styles.textMembro}>
-            Matheus Carvalho é o mestre da lógica na nossa equipe. Com um
-            conhecimento profundo em diversas tecnologias, ele é a referência na
-            resolução de problemas complexos. Sua expertise técnica é
-            indispensável para o nosso sucesso.
-          </p>
-        </div>
-
-
-        <div className={styles.backImg}>
-          <Image className={styles.fotoMembro} src={pravel} width={80} height={190} />
-        </div>
-        <div className={styles.cardDetalhe}>
-          <h4 className={styles.nomeMembro}>Matheus Gomes</h4>
-          <p className={styles.membro}>membro</p>
-          <p className={styles.textMembro}>
-            Matheus Gomes é um verdadeiro gênio em programação. Com um talento
-            excepcional para desenvolver soluções, ele combina brilhantismo
-            técnico com uma abordagem colaborativa que enriquece todo o time.
-          </p>
-        </div>
-
-
-        <div className={styles.backImg}>
-          <Image className={styles.fotoMembro} src={zambon} width={80} height={200} />
-        </div>
-        <div className={styles.cardDetalheZ}>
-          <h4 className={styles.nomeMembro}>Matheus Zambon</h4>
-          <p className={styles.membro}>membro</p>
-          <p className={styles.textMembro}>
-            Matheus Zambon é o membro mais animado e o melhor em trabalho em
-            equipe. Sua energia contagiante e habilidade para colaborar
-            eficazmente com todos tornam ele um elemento essencial para o nosso
-            sucesso coletivo.
-          </p>
-        </div>
       </div>
     </div>
   );
 }
+
+
