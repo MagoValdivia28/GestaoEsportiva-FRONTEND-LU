6acde0e48bfa55281de8b71ae5cc7c15f3f0a2f7
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-11-03 21:26:42 -0300

    Refactor feedback page: add MVV icons and update styles

--- src/app/feedback/page.jsx
+++ src/app/feedback/page.jsx
@@ -1,4 +1,9 @@
 import styles from './page.module.css';
+import { IoInformationCircleOutline } from "react-icons/io5";
+import { TbCoins } from "react-icons/tb";
+import { CgProfile } from "react-icons/cg";
+import { GiTennisCourt } from "react-icons/gi";
+
 
 const FeedBack = () => {
     return (
@@ -26,7 +31,34 @@ const FeedBack = () => {
                 </div>
             </div>
 
-            <div className={styles.MVV_ontainer}>
+            <div className={styles.MVV_container}>
+                <div className={styles.iconMVV}>
+                    <IoInformationCircleOutline className={styles.icon} />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Consequatur dicta molestiae earum provident inventore accusamus
+                        deserunt neque ducimus, voluptas ullam ut maiores voluptatum soluta!
+                        Modi suscipit facilis soluta in voluptatum.</p>
+                </div>
+
+                <div className={styles.iconMVV}>
+                    <TbCoins className={styles.icon} />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Consequatur dicta molestiae earum provident inventore accusamus
+                        deserunt neque ducimus, voluptas ullam ut maiores voluptatum soluta!
+                        Modi suscipit facilis soluta in voluptatum.</p>
+                </div>
+
+                <div className={styles.iconMVV}>
+                    <CgProfile className={styles.icon} />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Consequatur dicta molestiae earum provident inventore accusamus
+                        deserunt neque ducimus, voluptas ullam ut maiores voluptatum soluta!
+                        Modi suscipit facilis soluta in voluptatum
+                    </p>
+                </div>
+            </div>
+
+            <div className={styles.feedback_container}>
 
             </div>
         </main>
