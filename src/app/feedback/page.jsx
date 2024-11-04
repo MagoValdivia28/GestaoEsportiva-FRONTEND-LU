780f461f291675eeae5b4136855fb3bccb0f7d4c
Felipe da Silva Pedro <123407336+MagoValdivia28@users.noreply.github.com>
2024-11-04 17:17:35 -0300

    Refatorar estilos CSS: remover classes desnecessárias e reorganizar seções para melhor legibilidade

--- src/app/feedback/page.jsx
+++ src/app/feedback/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import styles from './page.module.css';
 import { IoInformationCircleOutline } from "react-icons/io5";
@@ -9,82 +9,51 @@ import { useState } from 'react';
 import emailjs from '@emailjs/browser';
 import Header from '../components/header/header';
 
-
 const FeedBack = () => {
     const [feedBack, setFeedBack] = useState('');
 
     const sendFeedBack = () => {
-        if (feedBack == '') {
+        if (!feedBack) {
             alert('O campo de feedback não pode estar vazio');
             return;
         }
-        emailjs.send('service_1q7z5qf', 'template_1q7z5qf', { message: feedBack }, 'user_1q7z5qf')
-    }
-
+        emailjs.send('service_1q7z5qf', 'template_1q7z5qf', { message: feedBack }, 'user_1q7z5qf');
+    };
 
     return (
-        <main className={styles.main_container}>
+        <main className={styles.mainContainer}>
             <Header />
-            <div className={styles.about_container}>
-                <div className={styles.info_about_container}>
-                    <h1>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem maiores repellat sunt autem
-                        explicabo quod voluptates aliquid ducimus
-                        eligendi dolores nam dolore eveniet quaerat, natus fugiat ipsam officia rerum placeat.
+            <section className={styles.aboutSection}>
+                <div className={styles.aboutText}>
+                    <h1 className={styles.title}>
+                        <span className={styles.titleRed}>gerenciamento</span>
+                        <span className={styles.titleBlack}>dos FeedBacks</span>
                     </h1>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem maiores repellat sunt autem
-                        explicabo quod voluptates aliquid ducimus
-                        eligendi dolores nam dolore eveniet quaerat, natus fugiat ipsam officia rerum placeat.
+                    <p>pagina destinada ao gerenciamento de feedbacks e opinioes dos clientes, com o objetivo de melhorar a qualidade dos nossos serviços e produtos.
                     </p>
                 </div>
+                <div className={styles.aboutImage}></div>
+            </section>
 
-                <div className={styles.img_about_container}>
-
-                </div>
-            </div>
-
-            <div className={styles.MVV_container}>
-                <div className={styles.iconMVV}>
+            <section className={styles.valuesSection}>
+                <div className={styles.valueCard}>
                     <IoInformationCircleOutline className={styles.icon} />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Consequatur dicta molestiae earum provident inventore accusamus
-                        deserunt neque ducimus, voluptas ullam ut maiores voluptatum soluta!
-                        Modi suscipit facilis soluta in voluptatum.</p>
+                    <p>Valores essenciais e o que nos motiva no dia a dia.</p>
                 </div>
-
-                <div className={styles.iconMVV}>
+                <div className={styles.valueCard}>
                     <TbCoins className={styles.icon} />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Consequatur dicta molestiae earum provident inventore accusamus
-                        deserunt neque ducimus, voluptas ullam ut maiores voluptatum soluta!
-                        Modi suscipit facilis soluta in voluptatum.</p>
+                    <p>Compromisso com a excelência e desenvolvimento sustentável.</p>
                 </div>
-
-                <div className={styles.iconMVV}>
+                <div className={styles.valueCard}>
                     <CgProfile className={styles.icon} />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Consequatur dicta molestiae earum provident inventore accusamus
-                        deserunt neque ducimus, voluptas ullam ut maiores voluptatum soluta!
-                        Modi suscipit facilis soluta in voluptatum
-                    </p>
+                    <p>Respeito, transparência e trabalho em equipe.</p>
                 </div>
-            </div>
+            </section>
 
-            <div className={styles.feedback_container}>
-                <GiTennisCourt className={styles.icon} />
-                <div className={styles.feedback_text} >
-                    <h1>Contate-nos</h1>
-                    <input
-                        type="text"
-                        placeholder="Escreva seu comentario para a AAPM!!"
-                        onChange={(e) => setFeedBack(e.target.value)}
-                        value={feedBack}
-                    />
-                </div>
-            </div >
-        </main >
-    )
-}
+        
+        </main>
+    );
+};
 
 export default FeedBack;
+
