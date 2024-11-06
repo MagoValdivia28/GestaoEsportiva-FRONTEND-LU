49fa4ae28320702f7cf77cc6010ed632584c974f
Felipe da Silva Pedro <123407336+MagoValdivia28@users.noreply.github.com>
2024-11-06 11:23:42 -0300

    feat: add modal for adding new modality and improve button styles

--- src/app/campeonato/[id]/atvesportiva/page.jsx
+++ src/app/campeonato/[id]/atvesportiva/page.jsx
@@ -1,18 +1,17 @@
-"use client"
+'use client';
+import { useRouter } from 'next/navigation'; // Importando useRouter
+import { useEffect, useState } from 'react';
 import styles from './page.module.css';
 import List_times from '@/src/app/components/list_times/list_times';
 import CardModality from '@/src/app/components/CardModality/page';
-import { useRouter } from 'next/navigation'; // Importando useRouter
-import { useEffect, useState } from 'react';
 import Header from '@/src/app/components/header/header';
 import { getAPI } from '@/src/actions/api';
 import { useParams } from 'next/navigation';
 
 const Gestaoesportes = () => {
   const { id } = useParams();
-  console.log(id);
-
   const [teams, setTeams] = useState([]);
+  const [showModal, setShowModal] = useState(false); // Novo estado para controle do modal
 
   useEffect(() => {
     const fetchTeams = async () => {
@@ -27,10 +26,12 @@ const Gestaoesportes = () => {
   }, []);
 
   const handleAddModality = () => {
-    // Handle the add modality action here, e.g., navigating to a form
-    console.log("Add modality button clicked!");
+    setShowModal(true); // Mostra o modal
   };
 
+  const handleCloseModal = () => {
+    setShowModal(false); // Fecha o modal
+  };
 
   return (
     <div className={styles.main_div}>
@@ -38,10 +39,8 @@ const Gestaoesportes = () => {
 
       <div className={styles.container}>
         <h2 className={styles.h2Title}> Placar do evento atual</h2>
-
         <List_times teams={teams} />
       </div>
-
 
       <div className={styles.modalidade_container}>
         <div className={styles.modalidade_container_title}>
@@ -54,15 +53,21 @@ const Gestaoesportes = () => {
           <CardModality title="Vôlei" />
           <CardModality title="Basquete" />
           <button onClick={handleAddModality} className={styles.addButton}>+</button>
-
         </div>
-
-
-        {/* dps o night faz o carrosel de modalidades */}
       </div>
 
+      {/* Pop-up modal */}
+      {showModal && (
+        <div className={styles.modalOverlay} onClick={handleCloseModal}>
+          <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+            <h2>Adicionar Nova Modalidade</h2>
+            <button onClick={handleCloseModal} className={styles.closeButton}>Fechar</button>
+            {/* Conteúdo do formulário de adição de modalidade */}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-export default Gestaoesportes;                                                                  
+export default Gestaoesportes;
