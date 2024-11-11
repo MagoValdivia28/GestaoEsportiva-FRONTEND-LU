eb9d29d7f11bec72a570bdf8b705ebb53bb3ebba
joaosantos564 <joaosantos@aluno.senai.br>
2024-11-11 07:33:15 -0300

    teste

---
+++ src/app/teste/page.jsx
@@ -0,0 +1,26 @@
+"use client"
+import React, { useState } from 'react';
+import CadastroPopup from '../components/PopUpEquipes';
+
+function App() {
+    // Estado para controlar a exibição do pop-up
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    // Função para abrir o pop-up
+    const openPopup = () => setIsPopupOpen(true);
+
+    // Função para fechar o pop-up
+    const closePopup = () => setIsPopupOpen(false);
+
+    return (
+        <div className="App">
+            {/* Botão para abrir o pop-up */}
+            <button onClick={openPopup}>Abrir Cadastro</button>
+
+            {/* Chama o componente CadastroPopup e passa as props */}
+            <CadastroPopup isOpen={isPopupOpen} onClose={closePopup} />
+        </div>
+    );
+}
+
+export default App;
