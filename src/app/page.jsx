f28cf1714290581a01bc28d4574abc6f2a513233
Matheus Zambon Da Costa <123407442+Matheuscosta06@users.noreply.github.com>
2024-11-13 14:14:21 -0300

    feat: atualizar rota de navegação para a página "Conheça-nos"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
   const router = useRouter();
 
   const handleConhecaNosClick = () => {
-    router.push('/'); // Navigate to the "Conheça-nos" page
+    router.push('/aapm'); // Navigate to the "Conheça-nos" page
   };
 
   const handleGerenciarCampeonatosClick = () => {
