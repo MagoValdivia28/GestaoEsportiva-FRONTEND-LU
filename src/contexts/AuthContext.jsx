c4ecc2a7b73d9c4f6eb6a3dea4651bdaff075bec
Matheus Henrique de Carvalho Côcco <matths.henrique@yahoo.com>
2024-11-13 15:57:26 -0300

    feat: aprimorar contexto de autenticação com gerenciamento de token e carregamento global

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -6,24 +6,60 @@ import { signIn } from '@/src/actions/user';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [acessToken, setAcessToken] = useState(null);
+    const apiURL = process.env.EXPO_PUBLIC_API_URL;
+    const [acessToken, setAcessToken] = useState('');
+    const [user, setUser] = useState('');
+    const [globalLoading, setGlobalLoading] = useState(false);
+    const [popUpMessage, setPopUpMessage] = useState(null);
 
+    useEffect(() => {
+        const loadingStoreData = async () => {
+            setGlobalLoading(true);
+            const storageToken = localStorage.getItem('@token');
 
-    const signInContext = async (nameParams, passwordParams) => {
-        setLoading(true);
-        const response = await signIn(nameParams, passwordParams);
-        setLoading(false);
-        if (response) {
-            setUser(response.user);
-            setAcessToken(response.token);
+            if (storageToken) {
+                try {
+                    const isLogged = await axios.post(`${apiURL}/users/refresh`, {
+                        refreshToken: JSON.parse(storageToken)
+                    });
+                    if (isLogged) {
+                        const userById = await axios.get(`${apiURL}/users/${isLogged.data.id}`, {
+                            headers: {
+                                Authorization: `Bearer ${isLogged.data.token}`
+                            }
+                        });
+                        setAcessToken(isLogged.data.token);
+                        const { password, ...userData } = userById.data;
+                        setUser(userData);
+                    }
+                } catch (error) {
+                    setPopUpMessage("Faça login novamente!");
+                    setTimeout(() => {
+                        setPopUpMessage(null);
+                    }, 3000);
+                    localStorage.clear();
+                }
+            }
+            setGlobalLoading(false);
+        };
+        loadingStoreData();
+    }, []);
+
+    const signIn = async (name, password) => {
+        const isLogged = await axios.post(`${apiURL}/users/login`, {
+            name: name,
+            password: password
+        });
+        if (isLogged) {
+            setAcessToken(isLogged.data.token);
+            const { password, ...userData } = isLogged.data.user;
+            setUser(userData);
+            localStorage.setItem('@token', JSON.stringify(isLogged.data.refreshToken.id));
         }
-        return response;
-    };
+    }
 
     return (
-        <AuthContext.Provider value={{ signInContext, loading, user, acessToken }}>
+        <AuthContext.Provider value={{  loading, acessToken, signIn, user, setUser, }}>
             {children}
         </AuthContext.Provider>
     );
