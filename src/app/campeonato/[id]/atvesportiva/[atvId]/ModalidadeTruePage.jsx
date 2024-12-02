5309c12835646d543fd06080e5c19500d72124f4
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-12-02 17:21:09 -0300

    feat: update API URL, enhance ModalidadeTruePage with winner selection and date generation functionality, and add modal styles

--- src/app/campeonato/[id]/atvesportiva/[atvId]/ModalidadeTruePage.jsx
+++ src/app/campeonato/[id]/atvesportiva/[atvId]/ModalidadeTruePage.jsx
@@ -1,30 +1,75 @@
-
 "use client";
 
 import styles from "./page.module.css";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { generateConfrontos } from "@/src/actions/api";
 import { getAPI } from "@/src/actions/api";
+import { updateConfronto } from "@/src/actions/api";
 import { useParams } from "next/navigation";
+import { AuthContext } from '@/src/contexts/AuthContext';
+import PopUpError from '@/src/app/components/PopUpError';
 
 const ModalidadeTruePage = ({ modalidade, teams }) => {
+  const { user, acessToken } = useContext(AuthContext);
   const [showModal, setShowModal] = useState(false);
+  const [dateInput, setDateInput] = useState(null);
   const [selectedWinner, setSelectedWinner] = useState(null);
   const [partidasData, setPartidasData] = useState(null);
+  const [popUp, setPopUp] = useState(false);
   const { atvId } = useParams();
+  const [popUpMessage, setPopUpMessage] = useState(null);
+  const [confrontosData, setConfrontosData] = useState(null);
 
   const handleAddWinner = () => {
     setShowModal(true);
   };
 
-  const handleSelectWinner = (team) => {
-    console.log(team);
-
-    setSelectedWinner(team);
+  const handleSelectWinner = async (confrontoParams) => {
+    console.log(confrontoParams);
+    const data = {
+      winner: true,
+      tie: false,
+      updAtIdUser: user.id
+    };
+    const response = await updateConfronto(confrontoParams.confrontoid, data, "acessToken");
+    console.log(response);
+    setSelectedWinner(confrontoParams.time);
     setShowModal(false);
   };
 
+  const handleGenerate = async () => {
+    if (!dateInput) {
+      setPopUpMessage({
+        status: "error",
+        message: "Por favor, selecione uma data.",
+      });
+      setTimeout(() => {
+        setPopUpMessage(null);
+      }, 2000);
+      return;
+    } else {
+      const response = await generateConfrontos(dateInput, '', user.id, atvId, acessToken);
+      if (response.message == "Confrontos gerados com sucesso") {
+        setPopUpMessage({
+          status: "sucess",
+          message: "Confrontos gerados com sucesso.",
+        });
+        setTimeout(() => {
+          setPopUpMessage(null);
+          window.location.reload();
+        }, 2000);
+      } else {
+        setPopUpMessage({
+          status: "error",
+          message: response.message,
+        });
+        setTimeout(() => {
+          setPopUpMessage(null);
+        }, 2000);
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchConfrontos = async () => {
@@ -34,21 +79,20 @@ const ModalidadeTruePage = ({ modalidade, teams }) => {
     };
     fetchConfrontos();
   }, []);
+
   return (
     <div>
       {!selectedWinner && (
         <div className={styles.addWinnerContainer}>
-          {
-            partidasData && partidasData.total > 0 ? (
-              <button className={styles.addWinnerButton} onClick={handleAddWinner}>
-                Adicionar Vencedor
-              </button>
-            ) : (
-              <button className={styles.addWinnerButton} onClick={handleAddWinner}>
-                Definir Data
-              </button>
-            )
-          }
+          {partidasData && partidasData.total > 0 ? (
+            <button className={styles.addWinnerButton} onClick={handleAddWinner}>
+              Adicionar Vencedor
+            </button>
+          ) : (
+            <button className={styles.addWinnerButton} onClick={() => setPopUp(true)}>
+              Definir Data
+            </button>
+          )}
         </div>
       )}
 
@@ -89,15 +133,15 @@ const ModalidadeTruePage = ({ modalidade, teams }) => {
       {showModal && (
         <div className={styles.modalOverlay}>
           <div className={styles.modal}>
-            <h3>Selecione o time vencedor</h3>
+            <h3 onClick={() => console.log(partidasData)}>Selecione o time vencedor</h3>
             <ul>
-              {teams.map((team, index) => (
+              {partidasData && partidasData.data.map((confronto) => (
                 <li
-                  key={index}
+                  key={confronto.confrontos[0].confrontoid}
                   className={styles.modalTeamItem}
-                  onClick={() => handleSelectWinner(team)}
+                  onClick={() => handleSelectWinner(confronto.confrontos[0])}
                 >
-                  {team.nome}
+                  {confronto.confronto.confrontos[0].time.nome}
                 </li>
               ))}
             </ul>
@@ -105,6 +149,19 @@ const ModalidadeTruePage = ({ modalidade, teams }) => {
           </div>
         </div>
       )}
+      {popUp && (
+        <div className={styles.modalOverlay}>
+          <div className={styles.modal}>
+            <h3 className={styles.modalTitle}>Selecione a data do jogo</h3>
+            <input onChange={(e) => setDateInput(e.target.value)} type="date" className={styles.modalInput} />
+            <div className={styles.modalButtons}>
+              <button className={styles.modalButton} onClick={handleGenerate}>Gerar</button>
+              <button className={styles.modalButton} onClick={() => setPopUp(false)}>Cancelar</button>
+            </div>
+          </div>
+        </div>
+      )}
+      {popUpMessage && <PopUpError error={popUpMessage} />}
     </div>
   );
 };
