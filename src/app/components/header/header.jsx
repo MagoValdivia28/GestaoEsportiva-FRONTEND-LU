aeaa197fac6e4f71c75eab11402ae7ffecd5cafd
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-12-04 00:04:42 -0300

    feat: adicionar responsividade ao título e ocultar cartão em telas menores; melhorar estilos do botão de perfil e popup

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -14,45 +14,47 @@ const Header = () => {
     const popupRef = useRef(null);
     const { getRefreshToken } = useContext(AuthContext);
 
-    const togglePopup = () => {
-        setShowPopup(!showPopup);
-    };
+    // Toggle visibility of the profile popup
+    const togglePopup = () => setShowPopup(prevState => !prevState);
 
+    // Close the popup when clicked outside
     const handleClickOutside = (event) => {
         if (popupRef.current && !popupRef.current.contains(event.target)) {
             setShowPopup(false);
         }
     };
 
+    // Register and clean up the event listener for clicks outside
     useEffect(() => {
         document.addEventListener('mousedown', handleClickOutside);
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
+        return () => document.removeEventListener('mousedown', handleClickOutside);
     }, []);
 
+    // Handle logout process
     const handleLogout = () => {
         const response = getRefreshToken();
         logout(response);
         localStorage.clear();
         window.location.reload();
-    }
+    };
 
     return (
         <header className={styles.header}>
-            <Link className={styles.buttonHome} href={"/"}>
-                <Image src={logo} className={styles.logo} width={130} height={130} />
+            <Link href="/" className={styles.buttonHome}>
+                <Image src={logo} alt="Logo" className={styles.logo} width={130} height={130} />
             </Link>
 
             <div className={styles.profileIcon}>
                 <button className={styles.profileButton} onClick={togglePopup}>
                     <FaUser size={20} />
                 </button>
-                {showPopup && (
-                    <div ref={popupRef} className={styles.popup}>
-                        <button className={styles.logoutButton} onClick={handleLogout}>Logout</button>
-                    </div>
-                )}
+                <div ref={popupRef} className={`${styles.popup} ${showPopup ? styles.show : ''}`}>
+                    {showPopup && (
+                        <button className={styles.logoutButton} onClick={handleLogout}>
+                            sair
+                        </button>
+                    )}
+                </div>
             </div>
         </header>
     );
