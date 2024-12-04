07738dfc42e39f62d2bcf564a13eb8b6c2f22ced
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-12-04 01:12:40 -0300

    feat: melhorar lógica e estilos do componente de confrontos com destaque para o vencedor

--- src/app/components/confrontos/confrontos.jsx
+++ src/app/components/confrontos/confrontos.jsx
@@ -1,4 +1,4 @@
-import styles from './confrontos.module.css';
+import styles from './confrontos.module.css'; 
 import { HiOutlineTrash } from "react-icons/hi";
 import { FaRegEdit } from "react-icons/fa";
 import { FaUserGroup } from "react-icons/fa6";
@@ -9,15 +9,12 @@ import { updateConfronto } from "@/src/actions/api";
 import { FaCrown } from "react-icons/fa";
 
 const Confrontos = ({ idPartida, data, confrontos }) => {
-    console.log(confrontos);
-
     const { user, acessToken } = useContext(AuthContext);
-    console.log(confrontos);
-
     const [selectedWinner, setSelectedWinner] = useState(null);
     const [selectedDefeat, setSelectedDefeat] = useState(null);
     const [winTimeName, setWinTimeName] = useState(null);
     const [popUpConfronto, setPopUpConfronto] = useState(false);
+
     const handleDelete = async () => {
         const response = await deleteConfronto(idPartida, acessToken);
         window.location.reload();
@@ -63,15 +60,14 @@ const Confrontos = ({ idPartida, data, confrontos }) => {
         }
     }
 
-
     const handleUpdate = async () => {
     };
+
     const handleWin = (team1, team2) => {
         setSelectedWinner(team1);
         setSelectedDefeat(team2);
         setWinTimeName(team1.time.nome);
     };
-
 
     const handlepopUpConfronto = (confrontos, idPartida) => {
         setPopUpConfronto(!popUpConfronto);
@@ -79,52 +75,61 @@ const Confrontos = ({ idPartida, data, confrontos }) => {
 
     return (
         <>
-            {
-                popUpConfronto && (
-                    confrontos.map((confronto, index) => (
-                        <div key={index} className={styles.confronto_containerBackground}>
-                            <div className={styles.confronto_container}>
-                                <div className={styles.sla}>
-                                    <button className={styles.exitButton} onClick={() => handlepopUpConfronto()}>X</button>
+            {popUpConfronto && (
+                confrontos.map((confronto, index) => (
+                    <div key={index} className={styles.confronto_containerBackground}>
+                        <div className={styles.confronto_container}>
+                            <div className={styles.sla}>
+                                <button className={styles.exitButton} onClick={() => handlepopUpConfronto()}>X</button>
+                            </div>
+                            <h2>Editar Confronto</h2>
+                            {confrontos[0].winner ? (
+                                <h3>{confrontos[0].time.nome} Venceu</h3>
+                            ) : confrontos[1].winner ? (
+                                <h3>{confrontos[1].time.nome} Venceu</h3>
+                            ) : (
+                                <h3>Empate</h3>
+                            )}
+                            <h3>{data}</h3>
+                            <div className={styles.time_container}>
+                                <div className={styles.group_container}>
+                                    <FaUserGroup className={styles.group} />
+                                    <h5>{confrontos[0].time.nome}</h5>
+                                    {confrontos[0].winner && <FaCrown className={styles.crown} />}
                                 </div>
-                                <h2>Editar Confronto</h2>
-                                {
-                                    confrontos[0].winner ? (
-                                        <h3>{confrontos[0].time.nome} Venceu</h3>
-                                    ) : confrontos[1].winner ? (
-                                        <h3>{confrontos[1].time.nome} Venceu</h3>
-                                    ) : (
-                                        <h3>Empate</h3>
-                                    )
-                                }
-                                <h3>{data}</h3>
-                                <div className={styles.time_container}>
-                                    <div className={styles.group_container}>
-                                        <FaUserGroup className={styles.group} />
-                                        <h5>{confrontos[0].time.nome}</h5>
-                                        {confrontos[0].winner && <FaCrown className={styles.crown} />}
-                                    </div>
-                                    <div className={styles.group_container}>
-                                        <FaUserGroup className={styles.group} />
-                                        <h5>{confrontos[1].time.nome}</h5>
-                                        {confrontos[1].winner && <FaCrown className={styles.crown} />}
-                                    </div>
-                                </div>
-                                <div className={styles.btnContainer}>
-                                    <div className={styles.buttonsContainer}>
-                                        <button className={styles.winButton} onClick={() => handleWin(confrontos[0], confrontos[1])}>{confrontos[0].time.nome} Venceu</button>
-                                        <button className={styles.drawButton} onClick={() => handleDraw(confrontos[0], confrontos[1])}>Empate</button>
-                                        <button className={styles.winButton} onClick={() => handleWin(confrontos[1], confrontos[0])}>{confrontos[1].time.nome} Venceu</button>
-                                    </div>
-                                    <button className={styles.saveButton} onClick={handleSelectWinner}>Salvar</button>
-
-
+                                <div className={styles.group_container}>
+                                    <FaUserGroup className={styles.group} />
+                                    <h5>{confrontos[1].time.nome}</h5>
+                                    {confrontos[1].winner && <FaCrown className={styles.crown} />}
                                 </div>
                             </div>
+                            <div className={styles.btnContainer}>
+                                <div className={styles.buttonsContainer}>
+                                    <button 
+                                        className={`${styles.winButton} ${selectedWinner === confrontos[0] ? styles.selected : ''}`} 
+                                        onClick={() => handleWin(confrontos[0], confrontos[1])}
+                                    >
+                                        {confrontos[0].time.nome} Venceu
+                                    </button>
+                                    <button 
+                                        className={styles.drawButton} 
+                                        onClick={() => handleDraw(confrontos[0], confrontos[1])}
+                                    >
+                                        Empate
+                                    </button>
+                                    <button 
+                                        className={`${styles.winButton} ${selectedWinner === confrontos[1] ? styles.selected : ''}`} 
+                                        onClick={() => handleWin(confrontos[1], confrontos[0])}
+                                    >
+                                        {confrontos[1].time.nome} Venceu
+                                    </button>
+                                </div>
+                                <button className={styles.saveButton} onClick={handleSelectWinner}>Salvar</button>
+                            </div>
                         </div>
-                    ))
-                )
-            }
+                    </div>
+                ))
+            )}
             <li className={styles.confronto}>
                 <div className={styles.date_container}>
                     <h3 className={styles.date}>{data}</h3>
@@ -133,7 +138,6 @@ const Confrontos = ({ idPartida, data, confrontos }) => {
                         <HiOutlineTrash onClick={() => handleDelete()} className={styles.icon} />
                     </div>
                 </div>
-
                 <div className={styles.time_container}>
                     <div className={styles.group_container}>
                         <FaUserGroup className={styles.group} />
@@ -141,12 +145,9 @@ const Confrontos = ({ idPartida, data, confrontos }) => {
                         {confrontos[0].winner && <FaCrown className={styles.crown} />}
                     </div>
                     <div className={styles.containerEmpate}>
-
-                        {
-                            confrontos[0].tie == true && confrontos[1].tie == true && (
-                                <h1 className={styles.versus}>Empate</h1>
-                            )
-                        }
+                        {confrontos[0].tie === true && confrontos[1].tie === true && (
+                            <h1 className={styles.versus}>Empate</h1>
+                        )}
                         <h1 className={styles.versus}>X</h1>
                     </div>
                     <div className={styles.group_container}>
@@ -155,9 +156,9 @@ const Confrontos = ({ idPartida, data, confrontos }) => {
                         {confrontos[1].winner && <FaCrown className={styles.crown} />}
                     </div>
                 </div>
-            </li >
+            </li>
         </>
     );
 }
 
-export default Confrontos
+export default Confrontos;
